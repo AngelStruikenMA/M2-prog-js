c8b4edaa95759298feff3717de4099cce57b766f
AngelStruiken <angelbonkie1234@gmail.com>
2023-11-21 12:43:30 +0100

    speed2

--- 01A/app.js
+++ 01A/app.js
@@ -10,3 +10,20 @@ class App
 }
 let app = new App();
 app.runApplicaion();
+let Variabelwaarde = 999
+let appNaam = "de speed slideshow"
+let versie = "Beta nummer 1.0 a broer"
+let versieDatum = "21-10-2023"
+let autheur = "de enige echte angel"
+let copyright = "als je mij na doet krijg je een boete jongeman"
+let distributeur = "alleen ik man..."
+let darkmode = true
+
+console.log("Variabelwaarde" + Variabelwaarde)
+console.log(appNaam)
+console.log(versie)
+console.log(versieDatum)
+console.log(autheur)
+console.log(copyright)
+console.log(distributeur)
+console.log(darkmode)
